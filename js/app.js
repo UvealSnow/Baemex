81643ae5ee5325f77b57a9d64aa45f9a056ce417
UvealSnow <kevhah92@gmail.com>
2016-07-15 10:11:47 -0500

    added scroll-reveal.js :squirrel:

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 $(document).ready (function () {
+	window.sr = ScrollReveal();
+
 	$("div[image]").imagefill();
 	$("#link").imagefill();
+	sr.reveal('div[navbar]', { duration: 1000 }, 200);
+	sr.reveal('.landing', { duration: 1000 }, 200);
+	sr.reveal('div[policy]', { duration: 1000, origin: 'left', reset: true}, 200);
+	sr.reveal('div[image]', { duration: 1000, origin: 'bottom' }, 200);
+	sr.reveal('div[contact]', { duration: 1000, origin: 'left' }, 200);
 });
