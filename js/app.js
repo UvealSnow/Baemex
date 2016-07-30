3c87c5581d91aa49d63ac48cc5c33a7d15ce962c
Kevin Avila <kevhah92@gmail.com>
2016-07-29 22:00:46 -0500

    made corrections

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 $(document).ready (function () {
 	window.sr = ScrollReveal();
 
-	$("div[image]").imagefill();
-	$("#link").imagefill();
+	
 	sr.reveal('div[navbar]', { duration: 1000 }, 200);
 	sr.reveal('.landing', { duration: 1000 }, 200);
 	sr.reveal('div[policy]', { duration: 1000, origin: 'left', reset: true}, 200);
 	sr.reveal('div[image]', { duration: 1000, origin: 'bottom' }, 200);
 	sr.reveal('div[contact]', { duration: 1000, origin: 'left' }, 200);
+
+	// $("div[image]").imagefill();
+	$("#link").imagefill();
 });
